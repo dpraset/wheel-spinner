224c26e4ff0c6a968af44ff9cd6fae2fd0a5fd0f
Daniel Prasetyo <121718321+dpraset@users.noreply.github.com>
2024-12-30 16:07:12 -0800

    add spin feature for inputs

--- wheel-spin.py
+++ wheel-spin.py
@@ -3,5 +3,11 @@ import random
 # initialize wheel 
 
 def wheel():
-    listname = input("Enter wheel inputs separated by semi-commas: ")
-    names = listname.split(';')
+    inputnames = input("Enter wheel inputs separated by semi-commas: ")
+    inputlist = inputnames.split(';')
+
+    while inputlist:
+        print("Spinning the wheel...\n")
+        winner = random.choice(inputlist)
+        print("The winner is:", winner)
+        inputlist.remove(winner)
